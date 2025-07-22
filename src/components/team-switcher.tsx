e869471d59495b4200cb475f63e785e41da66688
Talha <hpstar679@gmail.com>
2025-07-22 15:26:48 +0530

    smol change

--- src/components/team-switcher.tsx
+++ src/components/team-switcher.tsx
@@ -45,9 +45,8 @@ export function TeamSwitcher({
             <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
               <activeTeam.logo className="size-4" />
             </div>
-            <div className="grid flex-1 text-left text-sm leading-tight">
-              <span className="truncate font-semibold">{activeTeam.name}</span>
-              <span className="truncate text-xs">{activeTeam.plan}</span>
+            <div className="grid flex-1 text-left text-xl leading-tight">
+              <span className="truncate font-bold">{activeTeam.name}</span>
             </div>
             {/* <ChevronsUpDown className="ml-auto" /> */}
           </SidebarMenuButton>
